3e3bb638f6d926366da76d8f88ee97fc0bfc2f46
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2017-01-23 22:53:52 +0100

    ACF fields added

--- templates/partials/module-header.php
+++ templates/partials/module-header.php
@@ -14,9 +14,30 @@
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-      </ul>
+
+    
+        <?php
+        wp_nav_menu( array(
+          'menu'              => 'primary navigation',
+          'theme_location'    => 'primary navigation',
+          'depth'             => 2,
+          'container'         => 'ul',
+          'container_class'   => '',
+          'container_id'      => '',
+          'menu_class'        => 'nav navbar-nav navbar-right',
+          'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+          'walker'            => new wp_bootstrap_navwalker())
+        );
+        ?>
+
+         <form class="navbar-form navbar-right" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+          <div class="form-group">
+            <label class="sr-only" ><?php echo _x( 'Search for:', 'label', 'skatein' ); ?></label>
+            <input class="form-control" type="search"  required placeholder="<?php echo esc_attr_x( 'Skatepark, city or address', 'placeholder', 'skatein' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+          </div>
+          <button class="btn btn-primary" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+        </form>
+        
     </div>
   </div>
 </nav>
