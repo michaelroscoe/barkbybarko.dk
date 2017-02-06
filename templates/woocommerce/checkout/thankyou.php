8d33d95109f44872840a1e4860fe1ec73ae9aaa1
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2017-02-06 10:23:50 +0100

    Fixed thank you

--- templates/woocommerce/checkout/thankyou.php
+++ templates/woocommerce/checkout/thankyou.php
@@ -21,55 +21,62 @@ if ( ! defined( 'ABSPATH' ) ) {
 }?>
 
 
-    <div class="section section-info section-payment-recieved">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
+<div class="section section-info section-payment-recieved">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
 
-                </div>
-             </div>
-         </div>
-        </div>
-
-        <div class="section section-default">
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-offset-2 col-md-8">
-                <div class="well well-reciept">
-
-
-<?php if ( $order ) : ?>
-
-	<?php if ( $order->has_status( 'failed' ) ) : ?>
-
-		<p class="woocommerce-thankyou-order-failed"><?php _e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
-
-		<p class="woocommerce-thankyou-order-failed-actions">
-			<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php _e( 'Pay', 'woocommerce' ) ?></a>
-			<?php if ( is_user_logged_in() ) : ?>
-				<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php _e( 'My Account', 'woocommerce' ); ?></a>
-			<?php endif; ?>
-		</p>
-
-	<?php else : ?>
-
-		<h2 class="woocommerce-thankyou-order-received title">
-			<?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Thank you. Your order has been received.', 'woocommerce' ) ); ?>	
-		</h2>
-
-
-	<?php endif; ?>
-
-	<?php do_action( 'woocommerce_thankyou_' . $order->payment_method, $order->id ); ?>
-	<?php do_action( 'woocommerce_thankyou', $order->id ); ?>
-
-<?php else : ?>
-
-	<h2 class="woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Thank you. Your order has been received.', 'woocommerce' ), null ); ?></p>
-
-<?php endif; ?>
+			</div>
+		</div>
 	</div>
 </div>
-</div>
+
+<div class="section section-default">
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8">
+				<div class="well well-reciept">
+
+					<?php if ( $order ) : ?>
+
+						<?php if ( $order->has_status( 'failed' ) ) : ?>
+
+							<h1 class="woocommerce-thankyou-order-failed"><?php _e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></h1>
+
+							<p class="woocommerce-thankyou-order-failed-actions">
+								<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php _e( 'Pay', 'woocommerce' ) ?></a>
+								<?php if ( is_user_logged_in() ) : ?>
+									<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php _e( 'My Account', 'woocommerce' ); ?></a>
+								<?php endif; ?>
+							</p>
+
+						<?php else : ?>
+
+							<h1 class="woocommerce-thankyou-order-received title">
+								<?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Thank you. Your order has been received.', 'woocommerce' ) ); ?>	
+								</h1>
+
+						<?php endif; ?>
+
+						<?php do_action( 'woocommerce_thankyou_' . $order->payment_method, $order->id ); ?>
+						<?php do_action( 'woocommerce_thankyou', $order->id ); ?>
+
+					<?php else : ?>
+
+						<h1 class="woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Thank you. Your order has been received.', 'woocommerce' ), null ); ?></h1>
+
+					<?php endif; ?>
+					
+					<p>  Du har modtaget en email indeholdende loginoplysninger til siden her, hvor du under “min konto” altid vil kunne se aktive køb og anden relevant information.  </p>
+					<p>Bark by barko er en online hundeskole, hvor du kan få hjælp, søge viden, eller blot blive inspireret i en lang række emner som omhandler både træning, tricks, pleje og generel omgang med menneskets bedste ven. Hundeskolen tager også fat i en række adfærdsproblemer, og gør dig klogere på hundens generelle adfærd.</p>
+					<p><a class="btn btn-primary btn-lg" href="#">Kom igang nu. Klik her</a></p>
+					<small><em>Såfremt du ønsker at stoppe dit medlemskab efter den oplyste prøveperiode, skal du aktivt selv afmelde dette under ’Min konto’ på www.barkbybarko.dk inden næste fornyelse.</em></small>
+
+ 
+
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
